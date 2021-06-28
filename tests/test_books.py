e2d34df189eab7f66d55704795147adcc23024cb
Mykola Zhyhallo <mzhyhall@gmail.com>
2021-06-28 21:09:33 +0200

    More tests

--- tests/test_books.py
+++ tests/test_books.py
@@ -1,18 +1,25 @@
+import pytest
 from . import get_test_client
 
 from datetime import datetime
 
-def test_book():
-    author = "Test author"
-    title = "Test title"
-    publication_date = datetime.now().isoformat()
-    price = 100
+@pytest.mark.parametrize("author,title,description,publication_date,info,price,count_in_stock,discount", [
+    ("Test author", "Test title", "123132", datetime.now().isoformat(), "info", 100, 12, 10),
+    ("Test", "Test title", None, datetime.now().isoformat(), "info", 100, 12, 10),
+    ("author", "Test1212 title", "123132", datetime.now().isoformat(), None, 100, 12, 10),
+    ("author", "Test1212 title", "123132", datetime.now().isoformat(), None, 100, 0, 0),
+])
+def test_book(author,title,description,publication_date,info,price,count_in_stock,discount):
 
     data = {
         "author": author,
         "title": title,
         "publicationDate": publication_date,
-        "price": price
+        "price": price,
+        "description": description,
+        "info": info,
+        "count_in_stock": count_in_stock,
+        "discount": discount
     }
     response = get_test_client().post('/api/books/book', json=data)
     assert response.status_code == 200
@@ -25,8 +32,12 @@ def test_book():
     assert all_books[0]["id"] == book_id
     assert all_books[0]["author"] == author
     assert all_books[0]["title"] == title
-    assert all_books[0]["publicationDate"] == publication_date
+    assert all_books[0]["publicationDate"] == publication_date.isoformat()
     assert all_books[0]["price"] == price
+    assert all_books[0]["description"] == description
+    assert all_books[0]["info"] == info
+    assert all_books[0]["count_in_stock"] == count_in_stock
+    assert all_books[0]["discount"] == discount
 
     response = get_test_client().delete('/api/books/book', json={ "id": "id" })
     assert response.status_code == 200
